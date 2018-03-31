3be941452eb8aa5e01adce801b8e6cba6c6fdf3e
antokouf <antokoufis@gmail.com>
2018-03-31 12:46:07 +0300

    Creates main plugin file and header comments

---
+++ wp-backup-zip.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WpBackupZip
+ * Plugin URI: https://antonisk.com
+ * Description: A simple wordpress plugin folder  zipper for backups.
+ * Version: 1.0.0
+ * Author: Antonis Koufis
+ * Author URI: https://antonisk.com
+ * License: GPL2
+ */
